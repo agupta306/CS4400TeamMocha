ac21b4e19e2960d56d4b54cc078b02ebe676f416
Ashu Gupta <ashutoshgupta2k12@gmail.com>
2014-04-21 21:09:19 -0400

    Readded adminIcon.png, created Queries class (we'll populate this with empty methods for now. Make sure to add good specifications on what the methods take in and return), fixed small login bug

---
+++ TeamMochaGTMRS/src/Queries.java
@@ -0,0 +1,21 @@
+
+/**
+ * Library class for calling JDBC SQL queries
+ * @author ashutoshgupta
+ */
+public class Queries {
+	
+	/**
+	 * Logs into database with given username and password
+	 * @param username
+	 * @param password
+	 * @return 0 - patient logged in
+	 * 		   1 - doctor logged in
+	 * 		   2 - admin logged in
+	 * 		  -1 - login error
+	 */
+	public static int login(String username, String password) {
+		
+		return 0;
+	}
+}
